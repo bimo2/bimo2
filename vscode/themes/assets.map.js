add99da97dabd58755c57f4e54935949b34aa9e4
Bimal Bhagrath <48932917+bimo2@users.noreply.github.com>
2021-02-27 11:34:15 -0500

    Add all default vscode icons

---
+++ vscode/themes/assets.map.js
@@ -0,0 +1,217 @@
+export default {
+  file: '_file',
+  fileExtensions: {
+    // Android
+    'gradle': '_android',
+
+    // Apple
+    'pbxproj': '_apple',
+    'plist': '_apple',
+    'storyboard': '_apple',
+    'xcconfig': '_apple',
+    'xcframework': '_apple',
+    'xcscheme': '_apple',
+    'xcsettings': '_apple',
+    'xcworkspacedata': '_apple',
+
+    // CSS
+    'css': '_css',
+
+    // Dart
+    'dart': '_dart',
+
+    // Data
+    'sql': '_data',
+    'mysql': '_data',
+
+    // Go
+    'go': '_go',
+
+    // Google
+    'webmanifest': '_google',
+
+    // GraphQL
+    'gql': '_graphql',
+    'graphql': '_graphql',
+
+    // HTML
+    'html': '_html',
+    'xml': '_html',
+
+    // Image
+    'ico': '_image',
+    'jpg': '_image',
+    'jpeg': '_image',
+    'png': '_image',
+    'webp': '_image',
+
+    // Java
+    'java': '_java',
+
+    // JavaScript
+    'js': '_javascript',
+    'jsx': '_javascript',
+
+    // JSON
+    'json': '_json',
+
+    // Kotlin
+    'kt': '_kotlin',
+
+    // Markdown
+    'md': '_markdown',
+    'mdx': '_markdown',
+
+    // Package
+    'tar': '_package',
+    'tar.gz': '_package',
+    'zip': '_package',
+
+    // PDF
+    'pdf': '_pdf',
+
+    // Prisma
+    'prisma': '_prisma',
+
+    // PureScript
+    'purs': '_purescript',
+
+    // Ruby
+    'rb': '_ruby',
+    'html.erb': '_ruby',
+
+    // SASS
+    'less': '_sass',
+    'sass': '_sass',
+    'scss': '_sass',
+
+    // Scala
+    'sbt': '_scala',
+    'scala': '_scala',
+    'scala.html': '_scala',
+
+    // Shell
+    'sh': '_shell',
+
+    // Shopify
+    'liquid': '_shopify',
+
+    // SVG
+    'svg': '_svg',
+
+    // Swift
+    'swift': '_swift',
+
+    // TypeScript
+    'ts': '_typescript',
+    'tsx': '_typescript',
+
+    // YAML
+    'yaml': '_yaml',
+    'yml': '_yaml',
+  },
+  fileNames: {
+    // Android
+    'gradle.properties': '_android',
+
+    // Apple
+    'cartfile': '_apple',
+    'cartfile.resolved': '_apple',
+    'package.resolved': '_apple',
+    'podfile': '_apple',
+    'podfile.lock': '_apple',
+
+    // Babel
+    '.babelrc.js': '_babel',
+    '.babelrc.json': '_babel',
+    'babel.config.js': '_babel',
+    'babel.config.json': '_babel',
+
+    // Bitcoin
+    'bitcoin.conf': '_bitcoin',
+
+    // Codecov
+    'codecov.yaml': '_codecov',
+    'codecov.yml': '_codecov',
+
+    // DNA
+    'dna.json': '_dna',
+    'dna.yml': '_dna',
+    'dna.yaml': '_dna',
+
+    // ESLint
+    '.eslintignore': '_eslint',
+    '.eslintrc.js': '_eslint',
+    '.eslintrc.json': '_eslint',
+    '.eslintrc.yaml': '_eslint',
+    '.eslintrc.yml': '_eslint',
+
+    // Ethereum
+    'config.toml': '_ethereum',
+    'ethereum.toml': '_ethereum',
+
+    // Flutter
+    '.metadata': '_flutter',
+    'pubpec.yaml': '_flutter',
+    'pubspec.yml': '_flutter',
+    'pubspec.lock': '_flutter',
+
+    // GitHub
+    '.gitattributes': '_github',
+    '.gitconfig': '_github',
+    '.gitignore': '_github',
+    '.gitmodules': '_github',
+    '.gitkeep': '_github',
+    'license': '_github',
+
+    // Go
+    'go.mod': '_go',
+    'go.sum': '_go',
+
+    // Google
+    'manifest.json': '_google',
+    'robots.txt': '_google',
+
+    // Next.js
+    'next.config.js': '_nextjs',
+    'next-env.d.ts': '_nextjs',
+
+    // NGINX
+    'fastcgi.conf': '_nginx',
+    'mime.types': '_nginx',
+    'nginx.conf': '_nginx',
+    'proxy.conf': '_nginx',
+
+    // Node.js
+    '.npmignore': '_nodejs',
+    '.yarn-integrity': '_nodejs',
+    '.yarnrc': '_nodejs',
+    '.yarnrc.yaml': '_nodejs',
+    '.yarnrc.yml': '_nodejs',
+    'package.json': '_nodejs',
+    'package-lock.json': '_nodejs',
+    'yarn.lock': '_nodejs',
+
+    // Ruby
+    'gemfile': '_ruby',
+    'gemfile.lock': '_ruby',
+
+    // Prettier
+    '.prettierrc.js': '_prettier',
+    '.prettierrc.json': '_prettier',
+    '.prettierrc.yaml': '_prettier',
+    '.prettierrc.yml': '_prettier',
+
+    // TypeScript
+    'tsconfig.json': '_typescript',
+
+    // VSCode
+    '.editorconfig': '_vscode',
+
+    // Webpack
+    'webpack.config.js': '_webpack',
+
+    // XRP
+    'rippled.cfg': '_xrp',
+  },
+};
